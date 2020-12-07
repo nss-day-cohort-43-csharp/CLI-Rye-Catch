498136e4d25ad20b9b47436c0aae1ef7d0a91453
Austin Ring <aring97@gmail.com>
2020-12-07 15:58:54 -0600

    view blog details mostly complete

---
+++ TabloidCLI/UserInterfaceManagers/BlogDetailManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    internal class BlogDetailManager: IUserInterfaceManager
+    {
+        private IUserInterfaceManager _parentUI;
+        private BlogRepository _blogRepository;
+        private PostRepository _postRepository;
+        private TagRepository _tagRepository;
+        private int _blogId;
+
+        public BlogDetailManager(IUserInterfaceManager parentUI, string connectionString, int blogId)
+        {
+            _parentUI = parentUI;
+            _blogRepository = new BlogRepository(connectionString);
+            _postRepository = new PostRepository(connectionString);
+            _tagRepository = new TagRepository(connectionString);
+            _blogId = blogId;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Blog blog = _blogRepository.Get(_blogId);
+            Console.WriteLine($"{blog.Title} Details");
+            Console.WriteLine(" 1) View");
+            Console.WriteLine(" 0) Go Back");
+
+
+            Console.WriteLine("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    View();
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        private void View()
+        {
+            Blog blog = _blogRepository.Get(_blogId);
+            Console.WriteLine($"Title: {blog.Title}");
+            Console.WriteLine($"URL: {blog.Url}");
+            Console.WriteLine("Tags:");
+            foreach(Tag tag in blog.Tags)
+            {
+                Console.WriteLine(" " + tag);
+            }
+            Console.WriteLine();
+        }
+    }
+}
