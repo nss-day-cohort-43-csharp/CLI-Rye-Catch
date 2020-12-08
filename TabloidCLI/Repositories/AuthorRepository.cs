8b90e25cc85b9b607fb65f0d63dbf9f5a2f7cef8
ste163 <samtedwards@pm.me>
2020-12-08 15:45:10 -0500

    Deleting an author first deletes all the author info from AuthorTag table

--- TabloidCLI/Repositories/AuthorRepository.cs
+++ TabloidCLI/Repositories/AuthorRepository.cs
@@ -146,7 +146,10 @@ namespace TabloidCLI
                 conn.Open();
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = @"DELETE FROM Author WHERE id = @id";
+                    cmd.CommandText = @"DELETE FROM AuthorTag
+                                            WHERE AuthorId = @id
+                                        DELETE FROM Author
+                                            WHERE Author.id = @id";
                     cmd.Parameters.AddWithValue("@id", id);
 
                     cmd.ExecuteNonQuery();
